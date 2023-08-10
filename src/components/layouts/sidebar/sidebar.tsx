e318d40ae9e1ee8604779c5c3aa1e1150713bb31
mujibur-rohman <muji.official06@gmail.com>
2023-08-11 00:25:07 +0700

    dashboard item

--- src/components/layouts/sidebar/sidebar.tsx
+++ src/components/layouts/sidebar/sidebar.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { cn } from "@/lib/utils";
+import { LayoutDashboard, Newspaper } from "lucide-react";
 import styles from "./styles.module.css";
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   return (
     <aside className="hidden md:block">
       <div
@@ -29,62 +30,18 @@ const Sidebar = (props: Props) => {
           )}
         </div>
         <div className="flex flex-col h-full">
-          <div className="px-2 overflow-x-hidden scrollbar-thin scrollbar-track-white scrollbar-thumb-slate-100 h-[70%]">
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-            <h1>dwd</h1>
-          </div>
+          <ul className={styles.nav}>
+            <li
+              className={cn(styles["nav__item"], styles["nav__item--active"])}
+            >
+              <LayoutDashboard />
+              <span className={cn({ hidden: !open })}>Dashboard</span>
+            </li>
+            <li className={styles["nav__item"]}>
+              <Newspaper />
+              <span className={cn({ hidden: !open })}>My Articles</span>
+            </li>
+          </ul>
           <div className="text-sm bg-red-100 h-[30%] z-50 my-auto whitespace-pre w-full font-medium">
             <h1>dwd</h1>
             <h1>dwd</h1>

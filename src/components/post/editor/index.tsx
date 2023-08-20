e3ebc7c12ce3ead780930acd18fffd0168efdd5c
mujibur-rohman <muji.official06@gmail.com>
2023-08-20 16:09:58 +0700

    toolbar add extention

--- src/components/post/editor/index.tsx
+++ src/components/post/editor/index.tsx
@@ -1,18 +1,65 @@
-import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import Bold from "@tiptap/extension-bold";
+import Italic from "@tiptap/extension-italic";
+import Underline from "@tiptap/extension-underline";
+import Placeholder from "@tiptap/extension-placeholder";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import Heading from "@tiptap/extension-heading";
+import Strike from "@tiptap/extension-strike";
+import ListItem from "@tiptap/extension-list-item";
+import BulletList from "@tiptap/extension-bullet-list";
+import OrderedList from "@tiptap/extension-ordered-list";
 import Toolbar from "./toolbar/toolbar";
+import { useEditor, EditorContent } from "@tiptap/react";
+import { lowlight } from "lowlight/lib/core";
+// @ts-ignore
+import css from "highlight.js/lib/languages/css";
+import js from "highlight.js/lib/languages/javascript";
+import ts from "highlight.js/lib/languages/typescript";
+import html from "highlight.js/lib/languages/xml";
+import styles from "./styles.module.scss";
+
+lowlight.registerLanguage("html", html);
+lowlight.registerLanguage("css", css);
+lowlight.registerLanguage("js", js);
+lowlight.registerLanguage("ts", ts);
 
 type Props = {};
 
 function TextEditor({}: Props) {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "",
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      Bold,
+      Italic,
+      Underline,
+      Placeholder.configure({
+        placeholder: "Write content …",
+      }),
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
+      Heading,
+      Strike,
+      ListItem,
+      BulletList,
+      OrderedList,
+    ],
+    editorProps: {
+      attributes: {
+        class: styles["text-editor"],
+      },
+    },
+    content: ``,
   });
 
   return (
     <div>
-      <Toolbar />
+      <Toolbar editor={editor} />
       <EditorContent editor={editor} />
     </div>
   );
--- src/components/post/editor/toolbar/toolbar.tsx
+++ src/components/post/editor/toolbar/toolbar.tsx
@@ -1,8 +1,27 @@
-import { Paperclip } from "lucide-react";
+import {
+  BoldIcon,
+  CodeIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  Heading4Icon,
+  ImagePlus,
+  ItalicIcon,
+  ListIcon,
+  ListOrderedIcon,
+  Paperclip,
+  StrikethroughIcon,
+  UnderlineIcon,
+} from "lucide-react";
 import styles from "./styles.module.scss";
-type Props = {};
+import { Editor } from "@tiptap/react";
+import { cn } from "@/lib/utils";
 
-function Toolbar({}: Props) {
+type Props = {
+  editor: Editor | null;
+};
+
+function Toolbar({ editor }: Props) {
   return (
     <div className={styles.toolbar}>
       <div className={styles.left}>
@@ -10,8 +29,127 @@ function Toolbar({}: Props) {
           <Paperclip size={14} />
           <span>Youtube</span>
         </div>
+        <div className={styles.image}>
+          <ImagePlus size={14} />
+          <span>Image</span>
+        </div>
       </div>
-      <div className={styles.right}>s</div>
+      <div className={styles.right}>
+        <div
+          className={cn(styles["toolbar-item"], {
+            [styles.active]: editor?.isActive("bold"),
+          })}
+          onClick={() => {
+            editor?.chain().focus().toggleBold().run();
+          }}
+        >
+          <BoldIcon size={14} />
+        </div>
+        <div
+          className={cn(styles["toolbar-item"], {
+            [styles.active]: editor?.isActive("italic"),
+          })}
+          onClick={() => {
+            editor?.chain().focus().toggleItalic().run();
+          }}
+        >
+          <ItalicIcon size={14} />
+        </div>
+        <div
+          className={cn(styles["toolbar-item"], {
+            [styles.active]: editor?.isActive("underline"),
+          })}
+          onClick={() => {
+            editor?.chain().focus().toggleUnderline().run();
+          }}
+        >
+          <UnderlineIcon size={14} />
+        </div>
+        <div
+          className={cn(styles["toolbar-item"], {
+            [styles.active]: editor?.isActive("codeBlock"),
+          })}
+          onClick={() => {
+            editor?.chain().focus().toggleCodeBlock().run();
+          }}
+        >
+          <CodeIcon size={14} />
+        </div>
+        <div className={styles["toolbar-item__group"]}>
+          <div
+            className={cn(styles["toolbar-item__group-item"], {
+              [styles.active]: editor?.isActive("heading", { level: 1 }),
+            })}
+            onClick={() => {
+              editor?.chain().focus().toggleHeading({ level: 1 }).run();
+            }}
+          >
+            <Heading1Icon size={14} />
+          </div>
+          <div
+            className={cn(styles["toolbar-item__group-item"], {
+              [styles.active]: editor?.isActive("heading", { level: 2 }),
+            })}
+            onClick={() => {
+              editor?.chain().focus().toggleHeading({ level: 2 }).run();
+            }}
+          >
+            <Heading2Icon size={14} />
+          </div>
+          <div
+            className={cn(styles["toolbar-item__group-item"], {
+              [styles.active]: editor?.isActive("heading", { level: 3 }),
+            })}
+            onClick={() => {
+              editor?.chain().focus().toggleHeading({ level: 3 }).run();
+            }}
+          >
+            <Heading3Icon size={14} />
+          </div>
+          <div
+            className={cn(styles["toolbar-item__group-item"], {
+              [styles.active]: editor?.isActive("heading", { level: 4 }),
+            })}
+            onClick={() => {
+              editor?.chain().focus().toggleHeading({ level: 4 }).run();
+            }}
+          >
+            <Heading4Icon size={14} />
+          </div>
+        </div>
+        <div className={styles["toolbar-item__group"]}>
+          <div
+            className={cn(styles["toolbar-item__group-item"], {
+              [styles.active]: editor?.isActive("orderedList"),
+            })}
+            onClick={() => {
+              editor?.chain().focus().toggleOrderedList().run();
+            }}
+          >
+            <ListOrderedIcon size={14} />
+          </div>
+          <div
+            className={cn(styles["toolbar-item__group-item"], {
+              [styles.active]: editor?.isActive("bulletList"),
+            })}
+            onClick={() => {
+              editor?.chain().focus().toggleBulletList().run();
+            }}
+          >
+            <ListIcon size={14} />
+          </div>
+        </div>
+        <div
+          className={cn(styles["toolbar-item"], {
+            [styles.active]: editor?.isActive("strike"),
+          })}
+          onClick={() => {
+            editor?.chain().focus().toggleStrike().run();
+          }}
+        >
+          <StrikethroughIcon size={14} />
+        </div>
+      </div>
     </div>
   );
 }
